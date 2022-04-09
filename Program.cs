fd856f96eb563ac3120dfd1f3a15f0d655c73480
JoaoPedroCordoba <jp.8pedro86@gmail.com>
2022-04-08 21:30:55 -0300

    finalizado exercicio

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿double n1, n2;
+
+Console.WriteLine("Digite o primeiro número..:");
+n1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digite o segundo numero número..:");
+n2 = Convert.ToDouble(Console.ReadLine());
+
+if (n1 > n2)
+{
+    Console.WriteLine($"O maior número é {n1}");
+}
+else 
+{
+    Console.WriteLine($"O maior número é {n2}");
+}
+
